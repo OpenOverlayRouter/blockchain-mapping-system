689865aa8e451064a70a8c08ab0a0ad269ce6ae2
miquel <mik_ferri@hotmail.com>
2017-09-25 22:34:08 +0200

    state_test

--- Tests/state_test.py
+++ Tests/state_test.py
@@ -4,7 +4,7 @@ import json
 from state import State
 from config import Env
 from block import BlockHeader
-from utils import decode_hex, big_endian_to_int, encode_hex, parse_as_bin, parse_as_int, normalize_address
+from utils import decode_hex, big_endian_to_int, encode_hex, parse_as_bin, parse_as_int, normalize_address, int_to_big_endian
 import utils
 import json
 import random
@@ -30,26 +30,58 @@ def mk_basic_state(alloc, header=None, env=None, executing_on_head=False):
 
 
     state.block_number = header["number"]
-    state.gas_limit = header["gas_limit"]
     state.timestamp = header["timestamp"]
-    state.block_difficulty = header["difficulty"]
     state.commit()
 
     return state
 
+print("TEST STARTING")
+
 with open('../genesis.json') as data_file:
     data = json.load(data_file)
+
+print("INITIALAZING")
 
 alloc = data['alloc']
 state = mk_basic_state(alloc, None)
 addresses = []
 balances = []
 
+tx = {}
 for addr, data in alloc.items():
     addresses.append(addr)
-    balances.append(data['balance'])
+    balances.append(parse_as_int(data['balance']))
+    tx[addr] = 0
 
-N = 5000
-
+N = 50000
+print("DOING SOME RANDOM TX...")
 for i in range(N):
-    rand = random.randint(0, len(addresses)-1)
+    randFrom = random.randint(0, len(addresses)-1)
+    randTo = random.randint(0, len(addresses)-1)
+    randBalance = random.randint(0, 5000)
+    if(i%10000==0):
+        print("...")
+    balances[randFrom] = balances[randFrom] - parse_as_int(randBalance)
+    balances[randTo] = balances[randTo] + parse_as_int(randBalance)
+    state.increment_nonce(addresses[randFrom])
+    state.transfer_value(addresses[randFrom],addresses[randTo],parse_as_int(randBalance))
+    tx[addresses[randFrom]] = tx[addresses[randFrom]] - randBalance
+    tx[addresses[randTo]] = tx[addresses[randTo]] + randBalance
+
+print("TX PART FINISHED")
+
+err = False
+print("CHEKING VALUES...")
+for i in range(0,len(addresses)):
+    if(i%2000==0):
+        print("...")
+    if(balances[i] != state.get_balance(addresses[i])):
+        print(addresses[i])
+        print(balances[i])
+        print(state.get_balance(addresses[i]))
+        print ("TEST FAILED")
+        print(tx[addresses[i]])
+        err = True
+        break;
+if not err:
+    print ("TEST PASSED")
