9271c826b49f9bff35405dffd073cc89bf0999c1
Miquel Ferriol <mik_ferri@hotmail.com>
2017-09-28 22:14:12 +0200

    Balance methods

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 **rlp:** *A package for encoding and decoding data in and from Recursive Length Prefix notation* (tested v0.6.0)
 
 **PyCryptodome:** *Cryptographic library* (tested v3.4.7)
+
+**netaddr:** *IP library* (tested v0.7.19)
+
+**leveldb:** *Database library* (tested v0.194)
+
+**bitcoin:** *Keystore and bitcoin utils* (tested 1.1.42)
+
+**:** *Database library* (tested v0.194)
--- balance.py
+++ balance.py
@@ -1,37 +1,69 @@
 import ipaddress
 import rlp
 import netaddr
-from netaddr import IPNetwork, IPAddress
+from netaddr import IPNetwork, IPAddress, IPSet
+from utils import address, normalize_address
+
 
 class Balance(rlp.Serializable):
     fields = [
-        ('own_ips', [IPNetwork]),
-        ('delegated_ips', [IPNetwork]),
-        ('recieved_ips', [IPNetwork])
+        ('own_ips', IPSet),
+        ('delegated_ips', {address, IPSet}),
+        ('recieved_ips', {address, IPSet})
     ]
 
-    def __init__(self, own_ips=[], delegated_ips=[], recieved_ips=[]):
+    def __init__(self, own_ips=IPSet(), delegated_ips={}, recieved_ips={}):
         self.own_ips = own_ips
         self.delegated_ips = delegated_ips
         self.recieved_ips = recieved_ips
 
-    def add_own_ips(self,ips):
-        ip_list = []
-        for ip in self.own_ips:
-            ip_list.append(ip)
-        for ip in ips:
-            ip_list.append(ip)
-        self.own_ips = netaddr.cidr_merge(ip_list)
+    def add_own_ips(self, ips):
+        self.own_ips.add(ips)
 
-    def remove_own_ips(self,ips):
-        ip_list = []
-        for ip in self.own_ips:
-            ip_list.append(ip)
-        ip_list.remove(ips)
-        self.own_ips = netaddr.cidr_merge(ip_list)
+    def remove_own_ips(self, ips):
+        self.own_ips.remove(ips)
 
+    def add_delegated_ips(self, address, ips):
+        n_address = normalize_address(address)
+        if n_address in self.delegated_ips:
+            self.delegated_ips[n_address].add(ips)
+        else:
+            self.delegated_ips[n_address] = IPSet(ips)
 
-b = Balance([IPNetwork('10.105.205.8/32')])
+    def remove_delegated_ips(self, address, ips):
+        n_address = normalize_address(address)
+        self.delegated_ips[n_address].remove(ips)
+        if len(self.delegated_ips[n_address]) == 0:
+            self.delegated_ips.pop(n_address)
 
-b.add_own_ips([IPNetwork('10.105.205.8/25')])
-print b.own_ips
+    def add_recieved_ips(self, address, ips):
+        n_address = normalize_address(address)
+        if n_address in self.recieved_ips:
+            self.recieved_ips[n_address].add(ips)
+        else:
+            self.recieved_ips[n_address] = IPSet(ips)
+
+    def remove_recieved_ips(self, address, ips):
+        n_address = normalize_address(address)
+        self.recieved_ips[n_address].remove(ips)
+        if len(self.recieved_ips[n_address]) == 0:
+            self.recieved_ips.pop(n_address)
+
+    def in_own_ips(self,ips):
+        return self.own_ips.__contains__(ips)
+
+    def in_delegated_ips(self,ips):
+        for set in self.delegated_ips.itervalues():
+            if set.__contains__(ips):
+                return True
+        return False
+
+    def in_recieved_ips(self,ips):
+        for set in self.recieved_ips.itervalues():
+            if set.__contains__(ips):
+                return True
+        return False
+
+b = Balance()
+b.add_delegated_ips("3282791d6fd713f1e94f4bfd565eaa78b3a0599d",IPNetwork('10.105.205.8/29'))
+print b.in_delegated_ips(IPNetwork('10.105.205.8/28'))
