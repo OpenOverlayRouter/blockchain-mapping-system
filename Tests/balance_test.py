92bbc3ba3045635803e5f839d7abc68d91bc26bb
Miquel Ferriol <mik_ferri@hotmail.com>
2017-09-29 21:35:33 +0200

    Utils

--- Tests/balance_test.py
+++ Tests/balance_test.py
@@ -1,19 +1,35 @@
 from balance import Balance
 from netaddr import IPNetwork, IPAddress
 import json
+import random
+ip_list = list(IPNetwork('192.0.2.128/28'))
+random.shuffle(ip_list)
+N = 5000
 
-b = Balance()
-b.add_delegated_ips("3282791d6fd713f1e94f4bfd565eaa78b3a0599d", IPNetwork('10.105.205.8/29'))
+balance = Balance()
 
 with open('../genesis.json') as data_file:
     data = json.load(data_file)
-
+def create_rand_net():
+    a = random.randint(0,192)
+    b = random.randint(0,192)
+    c = random.randint(0,192)
+    d = random.randint(0,192)
+    e = random.randint(15,32)
+    return IPNetwork(str(a)+"."+str(b)+"."+str(c)+"."+str(d)+"/"+str(e))
+cont = 0
 alloc = data['alloc']
-
 addresses = []
-for addr, balance in alloc.items():
+for addr, bal in alloc.items():
     addresses.append(addr)
-print addresses
 
+for i in range(N):
+    balance.add_own_ips(create_rand_net())
+    net = create_rand_net()
+    if(balance.in_own_ips(net)):
+        cont = cont + 1
+        balance.remove_own_ips(net)
 
-print b.in_delegated_ips(IPNetwork('10.105.205.8/28'))
+print(cont)
+print(balance.own_ips)
+print(balance.recieved_ips)
