111ca03b383d2f446631044aec4c53d8c93c487b
Hamid <hlm6074@gmail.com>
2017-10-07 00:07:49 +0200

    fixes in chain_test2

--- Tests/chain_test2.py
+++ Tests/chain_test2.py
@@ -8,7 +8,7 @@ import transactions
 import trie
 import rlp
 from db import LevelDB, EphemDB
-import state
+from random import randint
 
 env = Env(LevelDB('./chain'))
 chain = Chain(genesis=mk_genesis_data(env), env=env)
@@ -17,18 +17,26 @@ prevnumber = chain.state.block_number
 
 for i in range(0, 20):
     print ("iteration " + str(i))
-    print ("blocknumber: " + str(prevnumber + 1))
     block = Block(BlockHeader(timestamp=int(time.time()), prevhash=prevhash, number=prevnumber + 1))
     t = trie.Trie(EphemDB())
     for j in range (0, 3):
         transaction = transactions.Transaction(i*3 + j, '', "192.172.9.1/28", 0, 'data', 1, 1, 1)
         block.transactions.append(transaction)
         t.update(rlp.encode(j), rlp.encode(transaction))
-    block.header.t1x_root = t.root_hash
+    block.header.tx_root = t.root_hash
     chain.add_block(block)
     prevhash=block.hash
     prevnumber = prevnumber + 1
     chain.process_time_queue()
+    time.sleep(2)  # para que timestamp (padre) != timestamp (hijo)
 
-chain.get_block_by_number(1)
+block = chain.get_block_by_number(2)
+print ("nonces for block " + str(2) + ": (" + str(len(block.transactions)) + " transactions in block)")
+for tx in block.transactions:
+    print(tx.nonce)
 
+rand = randint(0, 20)
+block = chain.get_block_by_number(rand)
+print ("nonces for block " + str(rand) + ": (" + str(len(block.transactions)) + " transactions in block)")
+for tx in block.transactions:
+    print(tx.nonce)
