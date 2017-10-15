c4cc404f4652eae49c789de958eee5c53755fd14
CodeEaterX <CodeEaterX@users.noreply.github.com>
2017-10-15 12:23:20 +0200

    test/transactions_test.py

--- Tests/transactions_test.py
+++ Tests/transactions_test.py
@@ -1,31 +1,30 @@
 import utils
 from transactions import Transaction
 import rlp
-from py_ecc.secp256k1 import privtopub
-from netaddr import IPNetwork
-import cPickle as pickle
-import os
-from ecdsa import SigningKey, SECP256k1
-import random
 
+#key = utils.random_privkey()
+key = '2ff2ea218ea9ee91ddd651065e63551ee14cf82ec70a2ca0fa71923da10dd97c'
+addr = utils.privtoaddr(key)
+print "SENDER:", addr.encode('hex')
 
-def mk_random_privkey():
-    k = hex(random.getrandbits(256))[2:-1].zfill(64)
-    assert len(k) == 64
-    return k.decode('hex')
+tx = Transaction(5,2,'54450450e24286143a35686ad77a7c851ada01a0', '192.152.0.0/16', 
+    ['1.1.1.1', '54dbb737eac5007103e729e9ab7ce64a6850a310',
+     '2.2.2.2', '89b44e4d3c81ede05d0f5de8d1a68f754d73d997',
+     '3.3.3.3', '3a1e02d9ea25349c47fe2e94f4265cd261c5c7ca'])
+# '3.3.3.3', '3a1e02d9ea25349c47fe2e94f4265cd261c5c7ca'
 
-priv = mk_random_privkey()
-addr = utils.privtoaddr(priv)
-pub = privtopub(priv)
-print(priv)
-print ("PUB")
-print (pub)
-print ("ADDR")
-print (addr.encode('HEX'))
-#self, nonce, to, value, type, data, v=0, r=0, s=0
-tx = Transaction(0,"3282791d6fd713f1e94f4bfd565eaa78b3a0599d","192.168.0.2/25",0,b'')
-tx.sign(priv)
-print(tx.hash.encode('HEX'))
-print (tx.ip_network)
-print(tx.hash.encode('HEX'))
-print(tx.sender.encode('HEX'))
+tx.sign(key)
+print "Tx ID", tx.hash.encode('hex')
+#print tx.ip_network
+#print utils.ip_to_bytes('192.152.0.0/16').encode('hex')
+assert tx.sender == addr
+print "Tx Sender OK" 
+assert tx.v in (27, 28)
+print "V OK"
+#print tx.metadata
+print tx.to_dict()
+rawtx = rlp.encode(tx).encode('hex')
+print rawtx
+tx2 = rlp.decode(rawtx.decode('hex'), Transaction)
+#print tx2.sender.encode('hex')
+print tx2.to_dict()
