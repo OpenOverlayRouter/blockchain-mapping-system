3a9e46b1c0443b5deb9d0de795c72ac5afd71a01
Hamid <hlm6074@gmail.com>
2017-11-07 21:43:29 +0100

    server and client created

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import chain_service
 from config import Env
 from db import LevelDB
 from chain_service import ChainService
+import select, socket, sys, Queue
+import struct
 
 inputs = []
 outputs = []
@@ -29,29 +31,73 @@ def init_consensus():
     return 0
 
 
-def main():
-    end = 0
-    chain = init_chain()
-    p2p = init_p2p()
-    consensus = init_consensus()
-    p2p.sync_chain()
-    lisp = init_lisp()
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = b''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
 
-    while not end:
-        object = select()
-        if (isinstance(object, Block)):
-            try:
-                chain.add_block(object)
-            except as e:
-                print("error adding block")
-                print(e)
-        elif (isinstance(object, Transaction)):
-            try:
-                chain.add_transaction(object)
-            except as e:
-                print("error adding transaction to pending list")
-                print(e)
-        elif (isinstance(object, Alocate):  # TODO: completar User_req
-            try:
-                User_req.create_transaction()
-                create_tx(User_req)
+class Server():
+    def __init__(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setblocking(0)
+        server.bind(('localhost', 50000))
+        server.listen(5)
+        inputs = [server]
+        outputs = []
+        message_queues = {}
+        while inputs:
+            readable, writable, exceptional = select.select(
+                inputs, outputs, inputs)
+            for s in readable:
+                if s is server:
+                    connection, client_address = s.accept()
+                    connection.setblocking(0)
+                    inputs.append(connection)
+                    message_queues[connection] = Queue.Queue()
+                else:
+                    # this method reads the amount of bytes indicated by the first 4 bytes of the buffer
+                    raw_msglen = recvall(s, 4) # size of data to read
+                    if not raw_msglen:
+                        print ("error while reading")
+                    msglen = struct.unpack('>I', raw_msglen)[0]
+                    data = recvall(s, msglen) # read the amount of data indicated by the first 4 bytes
+                    print("lo leido es " + str(data))
+                    if data:
+                        message_queues[s].put(data)
+                        if s not in outputs:
+                            outputs.append(s)
+                    else:
+                        if s in outputs:
+                            outputs.remove(s)
+                        inputs.remove(s)
+                        s.close()
+                        del message_queues[s]
+
+            for s in writable:
+                try:
+                    next_msg = message_queues[s].get_nowait()
+                except Queue.Empty:
+                    outputs.remove(s)
+                else:
+                    s.send(next_msg)
+
+            for s in exceptional:
+                inputs.remove(s)
+                if s in outputs:
+                    outputs.remove(s)
+                s.close()
+                del message_queues[s]
+
+
+
+def main():
+    server = Server()
+    client2 = Client()
+    client3 = Client()
+
+main()
