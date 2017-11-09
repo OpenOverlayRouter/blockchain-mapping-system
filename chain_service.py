627081b42da067ee5f5b96232aba4af510a3c740
miquel <mik_ferri@hotmail.com>
2017-11-09 21:18:23 +0100

    Main changes

--- chain_service.py
+++ chain_service.py
@@ -33,6 +33,7 @@ class ChainService():
 
     def add_transaction(self, tx):
         assert isinstance(tx, Transaction)
+
         # validate transaction
         try:
             # Transaction validation for broadcasting. Transaction is validated
