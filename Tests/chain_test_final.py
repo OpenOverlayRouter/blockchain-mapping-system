45f1c301a11fe308c29761127e29488d958320c6
miquel <mik_ferri@hotmail.com>
2017-11-30 22:57:19 +0100

    Merge branch 'master' of https://github.com/CodeEaterX/blockchain
    # Conflicts:
    #	Tests/chain_test1.py

--- Tests/chain_test_final.py
+++ Tests/chain_test_final.py
@@ -40,6 +40,8 @@ ip6List = list(IPNetwork('2001:db8:0:1:1:1:1:1/124'))
 i = 0
 nonce = 0
 blockNum = 1
+
+
 """
 print "Adding some Own IP TXs"
 
@@ -139,6 +141,7 @@ ip4List = list(IPNetwork('192.168.2.0/28'))
 ip6List = list(IPNetwork('2001:db9:0:1:1:1:1:1/124'))
 
 nonce = 64
+i = 0
 while i < len(ip4List):
     if(blockNum%2 == 0):
         tx = Transaction(nonce, 0, add3, 1, str(ip4List[i]))
@@ -156,6 +159,44 @@ while i < len(ip4List):
     blockNum += 1
     time.sleep(1)
 
+
+print("ADDRESS1")
+print "Own IPS"
+print(chain.get_own_ips(add1))
+print "Delegated IPS"
+print(chain.get_delegated_ips(add1))
+print "Received IPS"
+print(chain.get_received_ips(add1))
+print "Map Server"
+print(chain.get_map_server(add1))
+print "Locator"
+print(chain.get_locator(add1))
+
+print("--------------------------")
+print("ADDRESS2")
+print "Own IPS"
+print(chain.get_own_ips(add2))
+print "Delegated IPS"
+print(chain.get_delegated_ips(add2))
+print "Received IPS"
+print(chain.get_received_ips(add2))
+print "Map Server"
+print(chain.get_map_server(add2))
+print "Locator"
+print(chain.get_locator(add2))
+
+print("--------------------------")
+print("ADDRESS3")
+print "Own IPS"
+print(chain.get_own_ips(add3))
+print "Delegated IPS"
+print(chain.get_delegated_ips(add3))
+print "Received IPS"
+print(chain.get_received_ips(add3))
+print "Map Server"
+print(chain.get_map_server(add3))
+print "Locator"
+print(chain.get_locator(add3))
 
 ip4List = list(IPNetwork('193.168.2.0/28'))
 ip6List = list(IPNetwork('2002:db9:0:1:1:1:1:1/124'))
@@ -179,7 +220,26 @@ while i < len(ip4List):
     blockNum += 1
     time.sleep(1)
 
-chain.query_eid("193.168.2.0",0)
+
+tx1 = Transaction(nonce, 3, add1, 2, '1:1:1:1:1:1:1:1',[2, '1:1:1:1:1:1:1:1', 20, 230, 1, '1.1.1.1', 45, 50])
+tx1.sign(ks1.privkey)
+
+tx2 = Transaction(0, 3, add2, 2, '1:1:1:1:1:1:1:1',[2, '2:2:2:2:2:2:2:2', 20, 230, 1, '2.2.2.2', 45, 50])
+tx2.sign(ks2.privkey)
+
+tx3 = Transaction(0, 3, add3, 2, '1:1:1:1:1:1:1:1',[2, '3:3:3:3:3:3:3:3', 20, 230, 1, '3.3.3.3', 45, 50])
+tx3.sign(ks3.privkey)
+
+chain.add_pending_transaction(tx1)
+chain.add_pending_transaction(tx2)
+chain.add_pending_transaction(tx3)
+
+block = chain.create_block(add1)
+block.sign(ks1.privkey)
+chain.add_block(block)
+
+print(chain.query_eid("193.168.2.0",0).info.locator_records[0].locator)
+
 
 print("ADDRESS1")
 print "Own IPS"
