03a850b0380686c3391513b8beb8c5516df515ff
miquel <mik_ferri@hotmail.com>
2017-11-30 20:18:10 +0100

    Merge branch 'master' of https://github.com/CodeEaterX/blockchain
    # Conflicts:
    #	Tests/chain_test1.py

--- Tests/chain_test_final.py
+++ Tests/chain_test_final.py
@@ -1,24 +1,26 @@
-#from db import LevelDB,_EphemDB
+from db import LevelDB,_EphemDB
 import time
 from transactions import Transaction
 import copy
-#from config import Env
-#from chain import Chain
-#from genesis_helpers import mk_genesis_data
-#from apply import validate_transaction, apply_transaction
+from config import Env
+from chain import Chain
+from genesis_helpers import mk_genesis_data
+from apply import validate_transaction, apply_transaction
 from keystore import Keystore
 import rlp
 import netaddr
 from netaddr import IPNetwork, IPAddress, IPSet
 from utils import address, normalize_address
-#from chain_service import ChainService
+from chain_service import ChainService
 
 print "Loading the environment..."
-#db = LevelDB("./chain")
-#env = Env(db)
+db = LevelDB("./chain")
+env = Env(db)
 
+print "Loading chain..."
+chain = ChainService(env)
 
-print "Loading keystores"
+print "Loading keystores..."
 
 add1 = "094a2c9f5b46416b9b9bd9f1efa1f3a73d46cec2"
 add2 = "7719818983cb546d1badee634621dad4214cba25"
@@ -28,28 +30,99 @@ ks1 = Keystore.load("./keystore/094a2c9f5b46416b9b9bd9f1efa1f3a73d46cec2","TFG12
 ks2 = Keystore.load("./keystore/7719818983cb546d1badee634621dad4214cba25","TFG1234")
 ks3 = Keystore.load("./keystore/a3e04410f475b813c01ca77ff12cb277991e62d2","TFG1234")
 
-ipNet = IPNetwork('192.0.2.16/12')
+print "Starting test..."
 
-print "Starting test"
-ip4List = list(IPNetwork('192.0.2.0/15'))
-ip6List = list(IPNetwork('2001:db8:0:1:1:1:1:1/111'))
-print( len(ip4List),len(ip6List))
+print(chain.get_own_ips(add1))
+ip4List = list(IPNetwork('192.0.2.0/28'))
+ip6List = list(IPNetwork('2001:db8:0:1:1:1:1:1/124'))
+
 
 i = 0
+nonce = 0
+blockNum = 1
 while i < len(ip4List):
-
-    if((i+1)%2 == 0):
-        tx = Transaction(0+i, 0, add2, 1, str(ip4List[i]))
+    if(blockNum%2 == 0):
+        print("444444444444444444444444444444444444444444444444444444444444444444444444")
+        tx = Transaction(nonce, 0, add2, 1, str(ip4List[i]))
+        tx.sign(ks1.privkey)
+        chain.add_pending_transaction(tx)
+        i += 1
     else:
-        tx = Transaction(0+i+1, 0, add2, 2, str(ip6List[i]))
-    tx.sign(ks1)
-    chain.add_pending_transaction(tx)
+        print("666666666666666666666666666666666666666666666666666666666666666666666666")
+        tx = Transaction(nonce, 0, add2, 2, str(ip6List[i]))
+        tx.sign(ks1.privkey)
+        chain.add_pending_transaction(tx)
     block = chain.create_block(add1)
     block.sign(ks1.privkey)
     chain.add_block(block)
+    nonce += 1
+    blockNum += 1
+    time.sleep(1)
 
-tx1 = Transaction(0, 0, add2, 1, '192.152.0.0/12')
-tx1.sign(ks1.privkey)
+
+ip4List = list(IPNetwork('192.0.2.0/30'))
+ip6List = list(IPNetwork('2001:db8:0:1:1:1:1:1/126'))
+
+
+i = 0
+nonce = 0
+while i < len(ip4List):
+    if(blockNum%2 == 0):
+        print("444444444444444444444444444444444444444444444444444444444444444444444444")
+        tx = Transaction(nonce, 0, add2, 1, str(ip4List[i]))
+        tx.sign(ks1.privkey)
+        chain.add_pending_transaction(tx)
+        i += 1
+    else:
+        print("666666666666666666666666666666666666666666666666666666666666666666666666")
+        tx = Transaction(nonce, 0, add2, 2, str(ip6List[i]))
+        tx.sign(ks1.privkey)
+        chain.add_pending_transaction(tx)
+    block = chain.create_block(add1)
+    block.sign(ks1.privkey)
+    chain.add_block(block)
+    nonce += 1
+    blockNum += 1
+    time.sleep(1)
+
+print("ADDRESS1")
+print "Own IPS"
+print(chain.get_own_ips(add1))
+print "Delegated IPS"
+print(chain.get_delegated_ips(add1))
+print "Received IPS"
+print(chain.get_received_ips(add1))
+print "Map Server"
+print(chain.get_map_server(add1))
+print "Locator"
+print(chain.get_locator(add1))
+
+print("--------------------------")
+print("ADDRESS2")
+print "Own IPS"
+print(chain.get_own_ips(add2))
+print "Delegated IPS"
+print(chain.get_delegated_ips(add2))
+print "Received IPS"
+print(chain.get_received_ips(add2))
+print "Map Server"
+print(chain.get_map_server(add2))
+print "Locator"
+print(chain.get_locator(add2))
+
+print("--------------------------")
+print("ADDRESS3")
+print "Own IPS"
+print(chain.get_own_ips(add3))
+print "Delegated IPS"
+print(chain.get_delegated_ips(add3))
+print "Received IPS"
+print(chain.get_received_ips(add3))
+print "Map Server"
+print(chain.get_map_server(add3))
+print "Locator"
+print(chain.get_locator(add3))
+
 
 
 
