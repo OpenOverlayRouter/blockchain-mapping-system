eddafbbd3a1a742ca2d46e485bb2f29a4df80f8c
Hamid <hlm6074@gmail.com>
2017-12-18 20:06:09 +0100

    oor class created

--- blockchaincba.py
+++ blockchaincba.py
@@ -21,82 +21,10 @@ import glob
 import rlp
 from keystore import Keystore
 from consensus import Consensus
-from map_reply import MapReplyRecord, LocatorRecord, Response, MapServers
-from ipaddress import IPv4Network, IPv6Network, IPv4Address, IPv6Address
-from netaddr import IPAddress
 from p2p import P2P
 import sys
-import socket
 import fcntl, os
-import errno
-from time import sleep
-
-HOST = ''
-REC_PORT = 16001
-SND_PORT = 16002
-
-
-def open_sockets():
-    try:
-        rec_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        snd_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        print 'Socket created'
-    except socket.error, msg:
-        print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        sys.exit(1)
-    # Bind socket to local host and port
-    try:
-        rec_socket.bind((HOST, REC_PORT))
-    except socket.error:
-        print 'Bind failed.'
-        sys.exit(1)
-
-    print 'Socket bind complete in ports ' + str(REC_PORT) + ' and ' + str(SND_PORT)
-
-    return rec_socket, snd_socket
-
-
-# reads the fields nonce, AFI and the IP from the socket
-def read_socket(rec_socket):
-    try:
-        res = rec_socket.recv(26)
-        print(len(res))
-        print(res.encode('HEX'))
-        nonce = (struct.pack('>I', (int(struct.unpack("I", res[0:4])[0]))) + struct.pack('>I',(int(struct.unpack("I", res[4:8])[0])))).encode('HEX')
-        nonce = int(nonce,16)
-        afi = int(struct.unpack("H", res[8:10])[0])
-        address = ''
-        if (afi == 1):
-            address = str(IPAddress(int(res[10:14].encode('HEX'),16)))
-
-        elif (afi == 2):
-            address = str(IPAddress(int(res[10:26].encode('HEX'),16)))
-        else:
-            raise Exception('Incorrect AFI read from socket')
-        return nonce, afi, address
-
-    except socket.error, e:
-        err = e.args[0]
-        if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-            sleep(1)
-            print 'No data available'
-            return None,None,None
-        else:
-            # a "real" error occurred
-            print e
-
-
-def write_socket(res, snd_socket):
-    snd_socket.sendto(res, (HOST, SND_PORT))
-
-
-def test_map_reply():
-    locator = LocatorRecord(priority=0, weight=0, mpriority=0, mweight=0, unusedflags=0, LpR=0,
-                            locator=IPv4Address(u'192.168.0.1'))
-    locators = []
-    locators.append(locator)
-    reply = MapReplyRecord(eid_prefix=IPv4Network(u'192.168.1.0/24'), locator_records=locators)
-    print(reply.to_bitstream())
+from oor import Oor
 
 
 def init_chain():
@@ -125,6 +53,10 @@ def init_keystore(keys_dir='./Tests/keystore/'):
     return keys
 
 
+def init_oor():
+    return Oor()
+
+
 def run():
     chain = init_chain()
     p2p = init_p2p(chain.get_head_block().header.number)
@@ -135,6 +67,7 @@ def run():
     block_num = chain.get_head_block().header.number
     timestamp = chain.get_head_block().header.timestamp
     consensus.calculate_next_signer(myIPs, timestamp, block_num)
+    oor = init_oor()
 
     while(not end):
         
@@ -252,31 +185,11 @@ def run():
             p2p.stop()
             sys.exit(0)
 
-def read_request_and_process():
-    nonce, afi, address = read_socket(rec_socket)
-
-    if(nonce is not None and afi is not None and address is not None):
-        """
-        try:
-            res = chain.query_eid(address, nonce)
-        except Exception as e:
-            print e
-        """
-        locator = LocatorRecord(priority=0, weight=0, mpriority=0, mweight=0, unusedflags=0, LpR=0,
-                                locator=IPNetwork('192.168.0.1'))
-        locators = []
-        locators.append(locator)
-        reply = MapReplyRecord(eid_prefix=IPNetwork('192.168.1.0/24'), locator_records=locators)
-        reply = MapServers(info = [IPNetwork("192.168.1.42/32"),IPNetwork("192.168.0.2/32"),IPNetwork("192.168.0.3/32")])
-        r = Response(nonce=nonce, info=reply)
-        print(r.to_bytes().encode('HEX'))
-        write_socket(r.to_bytes(), snd_socket)
 
 
 
 if __name__ == "__main__":
     #run()
-
     """
     keys = init_keystore()
     chain = init_chain()
