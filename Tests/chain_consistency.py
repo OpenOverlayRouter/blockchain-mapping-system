8f5dfe0420c4c42c2ef41e056fd9e27ffbe8bdb4
Miquel Ferriol <mik_ferri@hotmail.com>
2017-12-22 22:23:56 +0100

    Merge branch 'master' of https://github.com/CodeEaterX/blockchain
    # Conflicts:
    #	blockchaincba.py

--- Tests/chain_consistency.py
+++ Tests/chain_consistency.py
@@ -1,30 +1,6 @@
+from db import LevelDB
 from config import Env
-from chain import Chain
-from genesis_helpers import mk_genesis_data
-from db import _EphemDB
-from block import Block, BlockHeader
-import time
-import transactions
-import trie
-import rlp
-import state
-from utils_test import get_rand_tx
-from db import LevelDB,_EphemDB
-import time
-from transactions import Transaction
-import copy
-from db import _EphemDB
-from config import Env
-from chain import Chain
-from genesis_helpers import mk_genesis_data
-from apply import validate_transaction, apply_transaction
-from keystore import Keystore
-import rlp
-import netaddr
-from netaddr import IPNetwork, IPAddress, IPSet
-from utils import address, normalize_address
 from chain_service import ChainService
-import sys
 
 db = LevelDB("./chain")
 env = Env(db)
