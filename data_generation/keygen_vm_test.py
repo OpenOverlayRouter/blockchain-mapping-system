63b2e8b0415bd11d7c3b2c5da5e4fb706b7a3407
Jordi Paillissé <25616138+jordipv@users.noreply.github.com>
2017-12-29 20:31:54 +0100

    key generation script

---
+++ data_generation/keygen_vm_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 13:47:54 2017
+
+@author: jordi
+"""
+
+from keystore import Keystore
+
+
+
+
+
+
+
+
+
+#if __name__ == "__main__":
+ 
+#for i in range(256):
+
+k = Keystore.new('TFG1234', None, 0, None)
+#k.path = '/jordi/Desktop/spyder-workspace/blockchaincba/Tests/keystore2/' + k.address.encode("HEX")
+k.save(k)
+print k.address.encode("HEX")
