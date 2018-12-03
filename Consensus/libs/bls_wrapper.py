39095bf04503044c7363bd6c3053f1a2ed61ec0b
Jan Manrique <jan.manrique@est.fib.upc.edu>
2018-12-03 22:09:13 +0100

    Completed bls wrapper and bls test

--- Consensus/libs/bls_wrapper.py
+++ Consensus/libs/bls_wrapper.py
@@ -1,4 +1,4 @@
-import os, sys, subprocess
+import os, re, sys, subprocess
 
 EXE='utils/bls.exe'
 
@@ -10,19 +10,87 @@ class Bls():
     def initialize(self):
         if not self._isInit:
             self._isInit = True
-            subprocess.check_call([EXE, "init"])
+            try:
+                out = subprocess.check_output([EXE, "init"])
+            except subprocess.CalledProcessError:
+                return False
 
-    def sign(self, m, i=0):
-        subprocess.check_call([EXE, "sign", "-m", m, "-id", str(i)])
+            m = re.search(r"secKey: (.+)\npubKey: (.+)", out)
 
-    def verify(self, m, i=0):
-        subprocess.check_call([EXE, "verify", "-m", m, "-id", str(i)])
+            if not m:
+                return False
 
-    def share(self, n, k):
-        subprocess.check_call([EXE, "share", "-n", str(n), "-k", str(k)])
+            self.secKey = m.group(1)
+            self.pubKey = m.group(2)
 
-    def recover(self, ids):
+        return True
+
+    def sign(self, m, sk=None):
+        if not self._isInit:
+            return False, ""
+        
+        if sk is None:
+            sk = self.secKey
+
+        try:
+            out = subprocess.check_output([EXE, "sign", "-m", m, "-sk", str(sk)])
+        except subprocess.CalledProcessError:
+            return False, ""
+
+        m = re.search(r"sMsg: (.+)", out)
+        return True, m.group(1)
+
+    def verify(self, m, sm, pk=None):
+        if not self._isInit:
+            return False
+        
+        if pk is None:
+            pk = self.pubKey
+
+        try:
+            subprocess.check_output([EXE, "verify", "-pk", pk, "-m", m, "-sm", sm])
+        except subprocess.CalledProcessError:
+            return False
+
+        return True
+
+    def share(self, k, ids):
+        result = []
+        cmd = [EXE, "share", "-sk", self.secKey, "-k", str(k), "-ids"]
+        for i in ids:
+            cmd.append(str(i))
+
+        try:
+            out = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError:
+            return False, []
+
+        matches = re.findall( r"share-0x\d: sk=(.+) pk=(.+)", out)
+        if len(matches) != len(ids):
+            return False, []
+
+        for index, id in enumerate(ids):
+            result.append({
+                "id": id,
+                "sk": matches[index][0],
+                "pk": matches[index][1]
+            })
+
+        return True, result
+
+    def recover(self, ids, sigs):
         cmd = [EXE, "recover", "-ids"]
         for i in ids:
-                cmd.append(str(i))
-        subprocess.check_call(cmd)
+            cmd.append(str(i))
+
+        cmd.append("-sigs")
+        for sig in sigs:
+            cmd.append(str(sig))
+
+        try:
+            out = subprocess.check_output(cmd)
+        except subprocess.CalledProcessError:
+            return False, ""
+
+        m = re.search(r"recovered: (.+)", out)
+        return True, m.group(1)
--- Consensus/main.py
+++ Consensus/main.py
@@ -4,25 +4,36 @@ from libs.bls_wrapper import Bls
 
 
 def main():
+
+    m = "hello bls threshold signature"
+    ids = [ 1, 5, 3]
+    k = 2
+
     bls = Bls()
     bls.initialize()
+    _, sm = bls.sign(m)
+    
+    if not bls.verify(m, sm):
+        print("Error verifying initial message")
+        return
 
+    _, shares = bls.share(k, ids)
+    
+    sigs = []
+    for share in shares:
+        _, aux = bls.sign(m, share["sk"])
+        if not bls.verify(m, aux, share["pk"]):
+            print("Error verifying message from id " + str(share["id"]))
+            return
 
-#       m = "hello bls threshold signature"
-#       n = 10
-#       ids = [1, 5, 3, 7]
-#       k = len(ids)
-#       init()
-#       sign(m)
-#       verify(m)
-#       share(n, k)
-#       for i in ids:
-#               sign(m, i)
-#               verify(m, i)
-#       subprocess.check_call(["rm", "sample/sign.txt"])
-#       recover(ids)
-#       verify(m)
+        sigs.append(aux)
+    
+    _, s = bls.recover(ids, sigs)
+    if not bls.verify(m, s):
+        print("Recover failed")
+        return
 
+    print("Recover is OK")
 
 
 if __name__ == "__main__":
