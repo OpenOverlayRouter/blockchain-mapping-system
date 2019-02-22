3550b87279cfcbe4d3f764169295eb44a010ac56
Jordi Paillisse <25616138+jordipv@users.noreply.github.com>
2019-02-22 15:42:16 +0100

    create dataset, keystore

---
+++ dataset/keygen_vm_test.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 20 13:47:54 2017
+
+@author: jordi
+"""
+
+from keystore import Keystore
+import sys
+
+node = sys.argv[1]
+
+print ("Generating keys for %s", node)
+
+try:    
+    addresses = open(node + '-addresses.txt', 'w')
+except Exception as e: 
+    print e
+    sys.exit(1)
+
+
+
+
+
+
+#if __name__ == "__main__":
+
+
+
+
+for i in range(250):
+
+    k = Keystore.new('TFG1234', None, 0, None)
+#k.path = '/jordi/Desktop/spyder-workspace/blockchaincba/Tests/keystore2/' + k.address.encode("HEX")
+    k.save(k)
+    addresses.write(k.address.encode("HEX")+'\n')
+    print k.address.encode("HEX")
+
+addresses.close()
