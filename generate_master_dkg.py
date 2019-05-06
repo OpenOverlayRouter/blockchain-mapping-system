afce82f6092246b6b833267b155cb834c7b8531a
Jordi Paillissé <jpvilanova@gmail.com>
2019-05-06 16:26:20 +0200

    fixes in genesis DKG generation

--- generate_master_dkg.py
+++ generate_master_dkg.py
@@ -16,6 +16,7 @@ DKG_NUMBER_PARTICIPANTS = 100
 
 #Make the random selection
 random_no = utils.compress_random_no_to_int('0xabff75259697c051af1299ce30dc74a108ff5d6bbcc24bb2c8c5dfa8864fa41f', 16)
+random_no_string = '0xabff75259697c051af1299ce30dc74a108ff5d6bbcc24bb2c8c5dfa8864fa41f'
 
 #Load master addresses        
 #List all addresses at the moment in the chain
@@ -43,7 +44,7 @@ for elem in dkg_group:
     print elem.encode('hex')
 
 #Generate DKG shares
-cons = consensus.Consensus(dkg_group, dkg_group, random_no, 0x00)
+cons = consensus.Consensus(dkg_group, dkg_group, random_no_string, 0x00, 0)
 to_send = cons.new_dkg(dkg_group, dkg_group)
 #Since we have given him all nodes, we can join the DKG shares directly
 
